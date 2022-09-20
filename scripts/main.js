48c358f3a8bf6c288476ad3b65418a1957925827
Kijete <113999043+Kijete@users.noreply.github.com>
2022-09-20 10:08:21 -0400

    Update main.js
    Continué

--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,7 @@ if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   const storedName = localStorage.getItem('name');
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `Allo ${storedName}!`;
 }
 myButton.onclick = () => {
   setUserName();
